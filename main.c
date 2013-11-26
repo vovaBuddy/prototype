3bd01b7f8388f2a945f595dbb006027665a67013
sdonline <dshevchenko@gmail.com>
2013-11-26 12:36:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,61 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>  
-#include "queue.h"
+#include <limits.h>
+#include <unistd.h>
 
-void event_generator(void* some)
+// Ограничение таблицы сигналов
+#define maxSignalNumber 64
+
+// TODO: Ограничение числа алгоритмов-подписчиков на один сигнал
+const maxSubscriberNumber = 8;
+
+// Сигнал
+typedef int signal_value_t;
+
+// Таблица сигналов
+static signal_value_t signalTable[maxSignalNumber+1]; // номера сигналов - положительные числа
+
+// Алгоритм
+typedef void (*algorithm_t) (signal_value_t);
+
+// Алгоритм1
+static void algorithm1(signal_value_t signal)
 {
-	srand ( time(NULL) );
-	int random_number = rand() % 4;
-	Queue *tmp = (Queue*) some;
-	put(tmp, random_number);
+    int i;
+
+    // декларация алиаса сигнала, например так
+    enum {
+	sensor1 = 1
+	,sensor2 = 12
+	,sensor31 = 25
+	,gear1 = 33
+	,gear2
+    };
+
+    // декларация сета или сложного объекта, пример
+    //const int motor[] = { sensor1, sensor2, sensor31, 0}; // 0 - несуществующий номер сигнала
+    const int motor[] = { gear1, gear2, 0};
+
+    if (sensor2 == signal) {
+	// цикл foreach по приводам мотора
+	for (i = 0; motor[i]; ++i) {
+	    signalTable[motor[i]] = 1;
+	}
+    }
+
+    printf("algorithm1 %d\n", signal);
 }
 
-void one_hendler()
+// Алгоритм2
+static void algorithm2(signal_value_t signal)
 {
-	printf("Hendling 1 event!\n");
+    // TODO
+    signalTable[signal] = signalTable[signal];
+    printf("algorithm2 %d\n", signal);
 }
 
-void two_hendler()
+// Алгоритм3
+static void algorithm3(signal_value_t signal)
 {
-	printf("Hendling 2 event!\n");
+    // TODO
+    signalTable[signal] = signalTable[signal];
+    printf("algorithm3 %d\n", signal);
 }
 
-void three_hendler()
-{
-	printf("Hendling 3 event!\n");
-}
+// Таблица алгоритмов
+static algorithm_t handlers[] = {
+    algorithm1
+    ,algorithm2
+    ,algorithm3
+};
+const handlersNum = sizeof(handlers)/sizeof(algorithm_t);
 
 int main()
 {
-	void (*fptr[10])();
-	
-	fptr[0] = event_generator;
-	fptr[1] = one_hendler;
-	fptr[2] = two_hendler;
-	fptr[3] = three_hendler;
-	
-	
-	int t;
-	Queue q;
-	init_queue(&q);
-	int i = 0;
-	
-	
-	while(++i < 20)
-	{
-		sleep(1);
-		printf("Step\n");
-		(*fptr[0])((void*)&q);
-		
-		t = get(&q);
-		if (t != 0)
-		{
-			(*fptr[t])();
-		}
-		
+    int i;
+    char buf[256];
+
+    while (1) {
+	long signal;
+
+	fgets(buf, sizeof buf, stdin);
+	signal = strtol(buf, NULL, 0);
+	if (signal < 0 || LONG_MAX == signal) continue;
+
+	for (i = 0; i < handlersNum; ++i) {
+	    handlers[i]((int)signal);
 	}
-	
-	return 0;
+    }
+
+    return 0;
 }
 
